a85ca882699673a071a1f5a7f6bf69df0d671262
Leebashiro <111729362+Leebashiro@users.noreply.github.com>
2023-04-27 22:08:20 +0200

    Mer scss, endret rigide størrelser til flex-grow for responsiv

--- gamestore/src/components/GameCard.js
+++ gamestore/src/components/GameCard.js
@@ -4,9 +4,9 @@ export default function GameCard({ name, background_image, slug, genres, genre})
   return (
     <Link to={`/games/${slug}`} className="gameCard">
       <img className="gameCard_image" src={background_image} alt={`${name} background`} />
-      <h3>{name}</h3>
-      <p>{genres?.map((genre) => genre.name).join(", ")}</p>
-      <p>{genre?.join(', ')}</p>
+      <h3 className="cardName">{name}</h3>
+      <p className="genres">{genres?.map((genre) => genre.name).join(", ")}</p>
+      <p className="genres">{genre?.join(', ')}</p>
     </Link>
   );
 }
